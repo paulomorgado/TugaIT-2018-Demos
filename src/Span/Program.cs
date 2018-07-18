b769e8b53713be54a6f985d28c7926ff4fc7d1e2
Paulo Morgado <paulo@paulomorgado.net>
2018-07-19 00:52:55 +0100

    Indexer

--- src/Span/Program.cs
+++ src/Span/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         int[] array = new int[10];
         Span<int> span = array.AsSpan();
-        for (int i = 0; i < 10; i++) array[i] = i;
+        for (int i = 0; i < 10; i++) span[i] = i;
         foreach (int v in span) WriteLine(v);
     }
 }
