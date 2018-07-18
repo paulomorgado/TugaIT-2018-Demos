48432a4bd90e290fe83d66d0e7f094a464958af0
Paulo Morgado <paulo@paulomorgado.net>
2018-07-18 16:11:19 +0100

    AsyncMain

--- src/GrabBag/Program.cs
+++ src/GrabBag/Program.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Linq;
-using System.Threading;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using static System.Console;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         int[] numbers = { 0b1, 0b10, 0b100, 0b1000, 0b1_0000, 0b10_0000 };
-        int result = SumOfSquares(numbers);
+        int result = await SumOfSquaresAsync(numbers);
         WriteLine(result);
     }
 
-    static int SumOfSquares(int[] numbers)
+    static Task<int> SumOfSquaresAsync(int[] numbers)
     {
-        return numbers.Select(i => i * i).Sum();
+        return Task.FromResult(numbers.Select(i => i * i).Sum());
     }
 }
