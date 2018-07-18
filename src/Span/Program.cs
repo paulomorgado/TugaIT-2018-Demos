18e75afda15de1658fb098de2ad5f9042fa9c06d
Paulo Morgado <paulo@paulomorgado.net>
2018-07-19 00:47:54 +0100

    Create Span<T> before  array elements initialization

--- src/Span/Program.cs
+++ src/Span/Program.cs
@@ -6,8 +6,8 @@ class Program
     static void Main(string[] args)
     {
         int[] array = new int[10];
-        for (int i = 0; i < 10; i++) array[i] = i;
         Span<int> span = array.AsSpan();
+        for (int i = 0; i < 10; i++) array[i] = i;
         foreach (int v in span) WriteLine(v);
     }
 }
