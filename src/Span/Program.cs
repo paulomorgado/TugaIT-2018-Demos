3d425f759fd1b92a175b85e8c25d4c7cbbc62a82
Paulo Morgado <paulo@paulomorgado.net>
2018-07-19 00:57:50 +0100

    Slice

--- src/Span/Program.cs
+++ src/Span/Program.cs
@@ -7,7 +7,8 @@ class Program
     {
         int[] array = new int[10];
         Span<int> span = array.AsSpan();
+        Span<int> slice = span.Slice(3, 5);
         for (int i = 0; i < 10; i++) span[i] = i;
-        foreach (int v in span) WriteLine(v);
+        foreach (int v in slice) WriteLine(v);
     }
 }
