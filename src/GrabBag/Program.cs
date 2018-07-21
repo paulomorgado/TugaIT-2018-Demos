795672f3fc54891280e065557a6435e94a707d6d
paulomorgado <470455+paulomorgado@users.noreply.github.com>
2018-07-21 09:38:31 +0100

    Non-trailing named arguments

--- src/GrabBag/Program.cs
+++ src/GrabBag/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using static System.Console;
 
@@ -7,13 +8,13 @@ class Program
     static async Task Main(string[] args)
     {
         int[] numbers = { 0b1, 0b10, 0b100, 0b_1000, 0b1_0000, 0b10_0000 };
-        int result = await SumOfSquaresAsync(numbers);
+        int result = await SumOfSquaresAsync(numbers, default);
         WriteLine(result);
     }
 
-    static Task<int> SumOfSquaresAsync(int[] numbers)
+    static Task<int> SumOfSquaresAsync(int[] numbers, CancellationToken cancellationToken)
     {
-        return Task.Run(Compute);
+        return Task.Run(function: Compute, cancellationToken);
 
         int Compute() => numbers.Select(i => i * i).Sum();
     }
