4fb49fefe4d62a242d4651cdc48a0b2e68837401
paulomorgado <470455+paulomorgado@users.noreply.github.com>
2018-07-21 10:00:33 +0100

    Leading digit separators

--- src/GrabBag/Program.cs
+++ src/GrabBag/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        int[] numbers = { 0b1, 0b10, 0b100, 0b1000, 0b1_0000, 0b10_0000 };
+        int[] numbers = { 0b1, 0b10, 0b100, 0b_1000, 0b1_0000, 0b10_0000 };
         int result = await SumOfSquaresAsync(numbers);
         WriteLine(result);
     }
